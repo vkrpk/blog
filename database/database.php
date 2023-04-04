1a6f3a5af5145e7032c306883499ea78f301fea4
vkrpk <victor.krupka@orange.fr>
2023-04-04 18:26:19 +0200

    db

--- database/database.php
+++ database/database.php
@@ -6,7 +6,7 @@
 $dbconfig = parse_ini_file(".env");
 
 try {
-    $pdo = new PDO("mysql:host=" . $dbconfig["HOST"] . ";dbname=" . $dbconfig["DBNAME"], $dbconfig["USER"], $dbconfig["PASSWD"], [
+    $pdo = new PDO("mysql:host=" . getenv("HOST") . ";dbname=" . getenv("DBNAME"), getenv("USER"), getenv("PASSWD"), [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     ]);
