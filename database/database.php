1437052d929688d6ddeab86a44ac31aba5818762
vkrpk <victor.krupka@orange.fr>
2023-04-04 17:56:42 +0200

    db

--- database/database.php
+++ database/database.php
@@ -1,8 +1,5 @@
 <?php
 
-// $dns = 'mysql:host=eu-cdbr-west-02.cleardb.net;dbname=heroku_3411905a033b8e9';
-// $user = 'b12d599e5af3c0';
-// $pwd = 'c83d637a';
 $dbconfig = parse_ini_file(".env");
 
 try {
@@ -11,6 +8,7 @@ try {
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     ]);
 } catch (PDOException $e) {
+    echo "l'erreur sql est : "+$dbconfig["HOST"];
     echo "ERROR: " . $e->getMessage();
 }
 
