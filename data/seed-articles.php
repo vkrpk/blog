e0a2e73149d293796981eb37a7d85c8f521ec1fd
vkrpk <victor.krupka@orange.fr>
2023-04-04 15:30:21 +0200

    ajout procfile

--- data/seed-articles.php
+++ data/seed-articles.php
@@ -5,8 +5,11 @@ $articles = json_decode(file_get_contents('./articles.json'), true);
 $dns = 'mysql:host=localhost:8889;dbname=blog';
 $user = 'root';
 $pwd = 'root';
+$dbconfig = parse_ini_file(".env");
 
-$pdo = new PDO($dns, $user, $pwd);
+// $pdo = new PDO($dns, $user, $pwd);
+$pdo = new PDO("mysql:host=" . $dbconfig["HOST"] . ";dbname=" . $dbconfig["DBNAME"], $dbconfig["USER"], $dbconfig["PASSWD"]);
+
 $statement = $pdo->prepare('
     INSERT INTO article (title, category, content, image) VALUES (:title, :category, :content, :image)
 ');
--- database/database.php
+++ database/database.php
@@ -1,17 +1,17 @@
 <?php
 
-$dns = 'mysql:host=eu-cdbr-west-02.cleardb.net;dbname=heroku_3411905a033b8e9';
-$user = 'b12d599e5af3c0';
-$pwd = 'c83d637a';
+// $dns = 'mysql:host=eu-cdbr-west-02.cleardb.net;dbname=heroku_3411905a033b8e9';
+// $user = 'b12d599e5af3c0';
+// $pwd = 'c83d637a';
+$dbconfig = parse_ini_file(".env");
 
 try {
-    $pdo = new PDO($dns, $user, $pwd, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    $pdo = new PDO("mysql:host=" . $dbconfig["HOST"] . ";dbname=" . $dbconfig["DBNAME"], $dbconfig["USER"], $dbconfig["PASSWD"], [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     ]);
 } catch (PDOException $e) {
-    echo "ERROR: ". $e->getMessage();
+    echo "ERROR: " . $e->getMessage();
 }
 
 return $pdo;
-
