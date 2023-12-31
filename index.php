d8488d7a604007120ac88713c0b67d514057253b
vkrpk <victor.krupka@orange.fr>
2023-12-31 17:59:45 +0100

    :rocket: Add favicon

--- index.php
+++ index.php
@@ -47,6 +47,12 @@
     <?php require_once 'includes/head.php' ?>
     <link rel="stylesheet" href="public/css/index.css">
     <title>BLOG APP</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/favicon/site.webmanifest">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
 </head>
 <body>
     <div class="container">
